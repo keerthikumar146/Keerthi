e98ac77b0eaa3fb70919daa87d48b7f5bfeb28c8
keerthikumar146 <keerthi.kumar@6dtech.co.in>
2022-01-05 17:29:07 +0530

    eclipse

--- eclipse/MulChat/src/com/test/mulchat/Server.java
+++ eclipse/MulChat/src/com/test/mulchat/Server.java
@@ -1,5 +1,18 @@
 package com.test.mulchat;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket s=new ServerSocket(8080);
+		
+		Socket so;
+		
+		while(true) {
+			 so=s.accept();
+			 System.out.println("new client request");
+		}
+	}
 }
