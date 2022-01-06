2b6f9f11304c114452340ad50a73869d4dd5bbee
keerthikumar146 <keerthi.kumar@6dtech.co.in>
2022-01-06 15:02:27 +0530

    eclipse

--- eclipse/MulChat/src/com/test/mulchat/Server.java
+++ eclipse/MulChat/src/com/test/mulchat/Server.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class Server {
@@ -18,7 +20,7 @@ public class Server {
 		
 		while(true) {
 			 so=s.accept();
-			 System.out.println("new client request" 	+ s);
+			 System.out.println("new client request " 	+ so);
 			 
 			 DataInputStream input=new DataInputStream(so.getInputStream());
 			 DataOutputStream output=new DataOutputStream(so.getOutputStream());
@@ -35,3 +37,62 @@ public class Server {
 		}
 	}
 }
+
+class ClientHandler implements Runnable {
+	Scanner sc = new Scanner(System.in);
+	private String name;
+	final DataInputStream dis;
+	final DataOutputStream dout;
+	Socket s;
+	boolean login;
+
+	public ClientHandler(String name, DataInputStream dis, DataOutputStream dout, Socket s) {
+		super();
+		this.name = name;
+		this.dis = dis;
+		this.dout = dout;
+		this.s = s;
+		this.login = true;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		String mes;
+		while (true) {
+			try {
+				mes=dis.readUTF();
+				System.out.println(mes);
+				
+				if(mes.equals("logout")) {
+					this.login=false;
+					this.s.close();
+					break;
+				}
+				
+				StringTokenizer stoken=new StringTokenizer(mes,"#");
+				String mestosend=stoken.nextToken();
+				String recp=stoken.nextToken();
+				
+				for(ClientHandler ch : Server.vector) {
+					if(ch.name.equals(recp) && ch.login==true) {
+						ch.dout.writeUTF(this.name+ " : "+mestosend);
+						break;
+					}
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+		}
+		try {
+			this.dis.close();
+			this.dout.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
+
